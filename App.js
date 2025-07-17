5cf159e216f01edaf7d89f19c3ec8a2f3c33bf93
cherrya1234 <xantavious@yahoo.com>
2025-07-17 10:57:26 -0400

    Update App.js

--- App.js
+++ App.js
@@ -13,17 +13,17 @@ export default function ReportMagic() {
     const formData = new FormData();
     fileList.forEach(file => formData.append("files", file));
 
-    const res = await axios.post("http://localhost:8000/api/upload", formData);
+    const res = await axios.post("https://report-magic-backend.onrender.com/api/upload", formData);
     setSummary(res.data.summary);
   };
 
   const handleAsk = async () => {
-    const res = await axios.post("http://localhost:8000/api/ask", { question });
+    const res = await axios.post("https://report-magic-backend.onrender.com/api/ask", { question });
     setAnswer(res.data.answer);
   };
 
   const handleExport = async () => {
-    const res = await axios.get("http://localhost:8000/api/export", { responseType: "blob" });
+    const res = await axios.get("https://report-magic-backend.onrender.com/api/export", { responseType: "blob" });
     const url = window.URL.createObjectURL(new Blob([res.data]));
     const link = document.createElement("a");
     link.href = url;
