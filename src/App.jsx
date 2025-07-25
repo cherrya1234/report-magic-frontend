2f3f379a3ada913547dcf5b105621d10acaf79c2
cherrya1234 <xantavious@yahoo.com>
2025-07-25 17:23:55 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UploadForm from "./UploadForm";
+import QAChat from "./QAChat";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>📊 Report Magic</h1>
+      <UploadForm />
+      <QAChat />
+    </div>
+  );
+}
+
+export default App;
